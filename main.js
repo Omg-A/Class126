0b0e2f9a8c80fcc54f9ccd1a89de4353c2ba1a99
Omg-A <67595078+Omg-A@users.noreply.github.com>
2021-09-27 11:53:42 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ leftWristX = 0;
 leftWristY = 0;
 rightWristX = 0;
 rightWristY = 0;
+scoreLeftWrist = 0;
 
 function preload(){
     song = loadSound("music.mp3");
@@ -18,8 +19,21 @@ function setup(){
     poseNet = ml5.poseNet(video, modelLoaded);
     poseNet.on('pose', gotPoses);
 }
+
 function draw(){
     image(video, 0, 0, 600, 500);
+
+    fill("red");
+    stroke("red");
+
+    if(scoreLeftWrist > 0.2){
+        circle(leftWristX, leftWristY, 20);
+        inNumberLeftWristY = Number(leftWristY);
+        remove_Decimals = floor(inNumberLeftWristY);
+        volume = remove_Decimals/500;
+        document.getElementById("volume").innerHTML = "Volume = " + volume;
+        song.setVolume(volume);
+    }
 }
 
 function play(){
@@ -39,6 +53,9 @@ function modelLoaded(){
 function gotPoses(results){
     if(results.length > 0){
         console.log(results);
+
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("Score of Left Wrist : " + scoreLeftWrist);
 
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
